e53939b0486527f950d4d118cd4cb4bf5593b4ae
nawedimroze <nawed.imroze@qapita.com>
2024-02-11 22:27:26 +0530

    Revert "Implements basic library manager with unit tests"
    This reverts commit 9be8f6f40964e2df8b00640fe8530f29f1f3187d.

--- library-management/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.WriteLine("Library Management System");
-
-// Capabilities I want in my Library Management System:
-// 1. Inventory Management of books
-// 2. Checkout and Return of books
-// 3. Library User Management
